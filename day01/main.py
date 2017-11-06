4ba494c17230e3c081ab3afacc8e28674f3328da
Anthony Schmieder <anthony@primatelabs.com>
2017-11-05 21:42:43 -0500

    add a main() method

--- day01/main.py
+++ day01/main.py
@@ -1,13 +1,13 @@
 import vector
 
-def parse_commands_from_input(input):
-  split = input.split(',')
+def parse_commands(string):
+  split = string.split(',')
   return map(lambda s: s.strip(), split)
 
-if __name__ == '__main__':
+def main():
   with open('input') as f:
     input = f.read()
-  commands = parse_commands_from_input(input)
+  commands = parse_commands(input)
 
   v = vector.VectorWithPathHistory(vector.Vector())
   ce = vector.CommandExecutor(v)
@@ -16,3 +16,6 @@ if __name__ == '__main__':
 
   print(vector.manhattan_distance_from_origin(v.position()))
   print(vector.manhattan_distance_from_origin(v.first_position_visited_twice()))
+
+if __name__ == '__main__':
+  main()

9127a5b004b8b6572ae2791953756af7ec369a94
Anthony Schmieder <anthony@primatelabs.com>
2017-11-07 20:47:37 -0500

    update main for day2 part 2

--- day02/main.py
+++ day02/main.py
@@ -1,31 +1,57 @@
 import vector
 
-# Vector origin is in the southwest corner.
-POSITION_TO_SYMBOL = {
-  2: ['1', '2', '3'],
-  1: ['4', '5', '6'],
-  0: ['7', '8', '9'],
+VECTOR_DOMAIN_1 = [
+  (0, 2), (1, 2), (2, 2),
+  (0, 1), (1, 1), (2, 1),
+  (0, 0), (1, 0), (2, 0),
+]
+
+POSITION_TO_SYMBOL_MAP_1 = {
+  2: "123",
+  1: "456",
+  0: "012",
+}
+
+VECTOR_DOMAIN_2 = [
+                  (2, 4),
+          (1, 3), (2, 3), (3, 3),
+  (0, 2), (1, 2), (2, 2), (3, 2), (4, 2),
+          (1, 1), (2, 1), (3, 1),
+                  (2, 0),
+]
+
+POSITION_TO_SYMBOL_MAP_2 = {
+  4: "  1  ",
+  3: " 234 ",
+  2: "56789",
+  1: " ABC ",
+  0: "  D  ",
 }
 
 def main():
   with open('input') as f:
-    command_lines = f.read().strip().split('\n')
+    process_input(VECTOR_DOMAIN_1, POSITION_TO_SYMBOL_MAP_1, f.read())
+  with open('input') as f:
+    process_input(VECTOR_DOMAIN_2, POSITION_TO_SYMBOL_MAP_2, f.read())
 
-  v = vector.BoundedVector(vector.Vector(1, 1), 2, 2)
+def process_input(domain, position_to_symbol_map, input):
+  command_lines = input.strip().split('\n')
+
+  v = vector.BoundedVector(vector.Vector(1, 1), vector.AllowedSquares(domain))
   cle = vector.CommandLineExecutor(vector.CommandExecutor(v))
 
   sequence = []
   for command_line in command_lines:
     cle.execute(command_line)
     position = cle.vector().position()
-    sequence.append(position_to_symbol(position))
+    sequence.append(position_to_symbol(position, position_to_symbol_map))
 
   print(''.join(sequence))
 
-def position_to_symbol(position):
+def position_to_symbol(position, position_to_symbol_map):
   x = position[0]
   y = position[1]
-  return POSITION_TO_SYMBOL[y][x]
+  return position_to_symbol_map[y][x]
 
 if __name__ == '__main__':
   main()

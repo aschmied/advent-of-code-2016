74759277a99e3d34af32d711b8f37bf8aa64cf65
Anthony Schmieder <anthony@primatelabs.com>
2017-11-11 11:12:39 -0500

    day 7: cleanup in ipv7

--- day07/ipv7.py
+++ day07/ipv7.py
@@ -1,3 +1,5 @@
+ABBA_LENGTH = 4
+
 class Parser(object):
   def __init__(self, string):
     self._sequences = []
@@ -9,16 +11,27 @@ class Parser(object):
   def _parse(self, string):
     for char in string:
       if char == '[':
-        if not self._is_hypertext:
-          self._finish_sequence(self._sequences)
-        self._is_hypertext = True
+        self._start_hypertext_sequence()
       elif char == ']':
-        if self._is_hypertext:
-          self._finish_sequence(self._hypertext_sequences)
-        self._is_hypertext = False
+        self._end_hypertext_sequence()
       else:
         self._append_char_to_sequence_builder(char)
-    
+    self._end_input()
+
+  def _start_hypertext_sequence(self):
+    if not self._is_hypertext:
+      self._finish_sequence(self._sequences)
+    self._is_hypertext = True
+
+  def _end_hypertext_sequence(self):
+    if self._is_hypertext:
+      self._finish_sequence(self._hypertext_sequences)
+    self._is_hypertext = False
+
+  def _append_char_to_sequence_builder(self, char):
+    self._sequence_builder += char
+
+  def _end_input(self):
     if self._is_hypertext:
       self._finish_sequence(self._hypertext_sequences)
     else:
@@ -30,9 +43,6 @@ class Parser(object):
     sequences.append(''.join(self._sequence_builder))
     self._sequence_builder = []
 
-  def _append_char_to_sequence_builder(self, char):
-    self._sequence_builder += char
-
   def sequences(self):
     return self._sequences
 
@@ -41,19 +51,13 @@ class Parser(object):
 
 def contains_abba_sequence(sequence):
   n = len(sequence)
-  for start_index in xrange(n):
-    end_index = start_index + 4
-    if end_index > n:
-      return False
+  for start_index in xrange(n - ABBA_LENGTH + 1):
+    end_index = start_index + ABBA_LENGTH
     if is_abba_sequence(sequence[start_index:end_index]):
       return True
   return False
 
 def is_abba_sequence(sequence):
-  if not len(sequence) == 4:
-    return False
-  if not sequence[0:2] == sequence[3:1:-1]:
-    return False
-  if sequence[0] == sequence[1]:
-    return False
-  return True
+  return (len(sequence) == ABBA_LENGTH and
+      sequence[0:2] == sequence[3:1:-1] and
+      sequence[0] != sequence[1])

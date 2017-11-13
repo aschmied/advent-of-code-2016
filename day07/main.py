cb3b788ff22787b2ca266942af87fb3d0b7ed11a
Anthony Schmieder <anthony@primatelabs.com>
2017-11-12 20:39:36 -0500

    day 7: update main for part 2 (ssl)

--- day07/main.py
+++ day07/main.py
@@ -5,12 +5,16 @@ def main():
     lines = f.readlines()
 
   number_address_supporting_tls = 0
+  number_address_supporting_ssl = 0
   for line in lines:
     address = ipv7.Address.parse(line.strip())
     if address.supports_tls():
       number_address_supporting_tls += 1
+    if address.supports_ssl():
+      number_address_supporting_ssl += 1
 
   print(number_address_supporting_tls)
+  print(number_address_supporting_ssl)
 
 if __name__ == '__main__':
   main()

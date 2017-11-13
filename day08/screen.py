f3838989e3beb06656e4e9eb1cd5b50ac549831c
Anthony Schmieder <anthony@primatelabs.com>
2017-11-12 22:00:32 -0500

    day 8: add RectInstruction

--- day08/screen.py
+++ day08/screen.py
@@ -15,3 +15,25 @@ class Screen(object):
 
   def _coords_to_index(self, x, y):
     return y * self._width + x
+
+class RectInstruction(object):
+  TEXT = 'rect'
+
+  @classmethod
+  def from_args(cls, list_of_args):
+    width_string, height_string = list_of_args[0].split('x')
+    width = int(width_string)
+    height = int(height_string)
+    return cls(width, height)
+
+  def __init__(self, width, height):
+    self._width = width
+    self._height = height
+
+  def execute(self, screen):
+    for y in xrange(self._height):
+      for x in xrange(self._width):
+        screen.set(x, y, ON_VALUE)
+
+  def text(self):
+    return self.TEXT
--- day08/test_screen.py
+++ day08/test_screen.py
@@ -2,22 +2,42 @@ import unittest
 
 import screen
 
-class TestScreen(unittest.TestCase):
-  def setUp(self):
-    self.WIDTH = 3
-    self.HEIGHT = 2
-    self.screen = screen.Screen(self.WIDTH, self.HEIGHT)
+WIDTH = 3
+HEIGHT = 2
 
+class ScreenTestCase(unittest.TestCase):
+  def setUp(self):
+    self.screen = screen.Screen(WIDTH, HEIGHT)
+
+  def assertOn(self, x, y):
+    self.assertEqual(self.screen.get(x, y), screen.ON_VALUE)
+
+  def assertOff(self, x, y):
+    self.assertEqual(self.screen.get(x, y), screen.OFF_VALUE)
+
+class TestScreen(ScreenTestCase):
   def test_default(self):
-    for y in xrange(self.HEIGHT):
-      for x in xrange(self.WIDTH):
-        self.assertState(x, y, screen.OFF_VALUE)
+    for y in xrange(HEIGHT):
+      for x in xrange(WIDTH):
+        self.assertOff(x, y)
 
   def test_set(self):
     self.screen.set(0, 0, screen.ON_VALUE)
-    self.assertState(0, 0, screen.ON_VALUE)
+    self.assertOn(0, 0)
     self.screen.set(0, 0, screen.OFF_VALUE)
-    self.assertState(0, 0, screen.OFF_VALUE)
+    self.assertOff(0, 0)
 
-  def assertState(self, x, y, value):
-    self.assertEqual(self.screen.get(x, y), value)
+class TestRectInstruction(ScreenTestCase):
+  def test_execute(self):
+    screen.RectInstruction(2, 1).execute(self.screen)
+    self.assertOn(0, 0)
+    self.assertOn(1, 0)
+    self.assertOff(2, 0)
+    self.assertOff(0, 1)
+    self.assertOff(1, 1)
+    self.assertOff(2, 1)
+
+  def test_parse(self):
+    inst = screen.RectInstruction.from_args(['3x2'])
+    self.assertEqual(inst._width, 3)
+    self.assertEqual(inst._height, 2)
